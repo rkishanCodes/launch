856d1970068ff52fdc854b6bbbf703f3d126257a
R Kishan <168627915+rkishanCodes@users.noreply.github.com>
2025-10-29 12:01:13 +0530

    added get

--- server/server.js
+++ server/server.js
@@ -6,6 +6,11 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
+
+app.get("/", (req, res) => {
+  res.send("✅ WebSocket server is running!");
+});
+
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
